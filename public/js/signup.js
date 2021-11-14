93f5a628b2a22b81e8229101de03744c4dd7f483
HanHsienLEI <lei800123@gmail.com>
2021-11-14 19:42:18 +0800

    add password confirmation functionality

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,17 @@
 const submitButton = document.querySelector("button");
-
+const confirmPassword = document.querySelector("#confirm-password")
+confirmPassword.addEventListener("input", e=>{
+  const password = document.querySelector("#password")
+  const passwordMatchedText = document.querySelector(".password-match")
+  const passwordNotMatchedText = document.querySelector(".password-not-match")
+  if(password.value === confirmPassword.value){
+  passwordMatchedText.removeAttribute("hidden")
+  passwordNotMatchedText.setAttribute("hidden", "hidden")
+  } else {
+    passwordNotMatchedText.removeAttribute("hidden")
+    passwordMatchedText.setAttribute("hidden", "hidden")
+  }
+})
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   const userName = document.querySelector("#name");
