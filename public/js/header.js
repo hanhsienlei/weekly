81843ef840852c120307c6104def6e6954f6403f
HanHsienLEI <lei800123@gmail.com>
2021-11-20 10:20:54 +0800

    add present page indicator on nav bar

--- public/js/header.js
+++ public/js/header.js
@@ -3,3 +3,18 @@ const signOut = ()=> {
   window.location.href="/signup"
 }
 
+const renderNavLink = () => {
+  const life = document.querySelector(".nav-link-life")
+  const horizon = document.querySelector(".nav-link-horizon")
+  const review = document.querySelector(".nav-link-review")
+  const currentPath = window.location.pathname
+  if(currentPath.includes("life")){
+    life.classList.add("active")
+  }else if(currentPath.includes("horizon")){
+    horizon.classList.add("active")
+  }else if(currentPath.includes("review")){
+    review.classList.add("active")
+  }
+}
+
+window.onload = renderNavLink()
